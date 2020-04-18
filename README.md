42e10d7f9e4cb40abdb6dcd21318261bc16d16af
Sean Klein <seanmarionklein@gmail.com>
2020-04-17 20:23:36 -0400

    Move workflows to nightly, format, add action buttons

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Josh Hates Closures
 
+![](https://github.com/smklein/josh_hates_closures/workflows/rust/badge.svg)
+
 Josh doesn't like closures, so I made an attribute which you can
 apply to closures. If you use this attribute on a closure, this
 crate squads up with the compiler and yells loudly.
--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,11 @@ pub fn not_a_closure(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let orig = item.clone();
     let expr = parse_macro_input!(item as syn::Expr);
 
-    let output = if let syn::Expr::Closure(expr_closure) = expr {
-        syn::Error::new(expr_closure.span(), "DON'T NEED NO STINKEN LAMBDA").to_compile_error().into()
+    if let syn::Expr::Closure(expr_closure) = expr {
+        syn::Error::new(expr_closure.span(), "DON'T NEED NO STINKEN LAMBDA")
+            .to_compile_error()
+            .into()
     } else {
         orig
-    };
-
-    output.into()
+    }
 }
